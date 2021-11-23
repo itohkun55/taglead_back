1fcd3efaeef485dfc1dcb61ae50c5d6444d85175
itohkun55 <itohkun55@hotmail.com>
2021-11-23 17:27:32 +0900

    入れ忘れ追加　１

---
+++ api/SendMessageLibrary.py
@@ -0,0 +1,13 @@
+HAS_NO_ACCOUNT="当サービスへのアカウント設定が終わっていません。しばらくお待ちください"
+
+HAS_NO_RESULT="検索の結果、該当する情報はありませんでした 　ID:"
+
+FORMATTED_INSERT_MEMO="定型入力に形式エラー "
+
+HAS_NO_PERMISSION="利用権限のない操作が行われました。 　ID:"
+
+INSERT_VARIDATION_ERROR="入力内容にエラーがありました。"
+
+HAS_NO_VALID_USER="ユーザーの設定が異常です。　ID:"
+
+HAS_NO_DATA="データが存在しません ID:"
