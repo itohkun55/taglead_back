7c79109ed03ab6fae5d811f56b03684559ba77ff
itohkun55 <itohkun55@hotmail.com>
2021-12-14 21:02:20 +0900

    ユーザー登録機能追加

--- taglead/settings.py
+++ taglead/settings.py
@@ -148,7 +148,7 @@ WSGI_APPLICATION = 'taglead.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-}
+#}
 
 # DATABASES = {
 #     'default': {
