abe041be71786f7580645860cc2fda475a5d6ecc
itohkun55 <itohkun55@hotmail.com>
2021-12-14 20:49:58 +0900

    ユーザー登録機能追加

--- taglead/settings.py
+++ taglead/settings.py
@@ -143,11 +143,11 @@ WSGI_APPLICATION = 'taglead.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 }
 
 # DATABASES = {
@@ -164,16 +164,16 @@ DATABASES = {
 #     }
 # }
 #本番セッティング
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.mysql',
-#          'NAME': 'itohkun55$taglead_db',                                                                                                                
-#          'USER': 'itohkun55',                      
-#          'PASSWORD': 'Masahikoitoh55',           
-#          'HOST': 'itohkun55.mysql.pythonanywhere-services.com',               
-#          #'PORT': '3360',                   
-#      }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'itohkun55$taglead_db',                                                                                                                
+         'USER': 'itohkun55',                      
+         'PASSWORD': 'Masahikoitoh55',           
+         'HOST': 'itohkun55.mysql.pythonanywhere-services.com',               
+         #'PORT': '3360',                   
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
